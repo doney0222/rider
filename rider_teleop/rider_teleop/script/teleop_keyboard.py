a7a5f5bea776495d11068898dff96248be72c771
Sung <doney0222@naver.com>
2021-10-15 15:21:57 +0900

    upload : add teleop

---
+++ rider_teleop/rider_teleop/script/teleop_keyboard.py
@@ -0,0 +1,195 @@
+"""
+조종 콘손 (키보드)
+
+"""
+
+import os
+import select
+import sys
+import rclpy
+from geometry_msgs.msg import Twist
+from rclpy.qos import QoSProfile
+
+if os.name == 'nt':
+    import msvcrt
+else:
+    import termios
+    import tty
+
+BASIC_MAX_LIN_VEL = 0.22
+BASIC_MAX_ANG_VEL = 2.84
+
+LIN_VEL_STEP_SIZE = 0.01
+ANG_VEL_STEP_SIZE = 0.1
+
+RIDER_MODEL = os.environ['RIDER_MODEL']
+
+msg = """
+라이더 조종 콘솔
+---------------------------
+        W
+   A    S    D
+        X
+
+W/X : 전진/후진 (선속도 : ~ 0.22)
+A/D : 좌/우 회전 (회전속도 : ~ 2.84)
+
+Space key, S : 정지
+
+CTRL-C : 종료
+"""
+
+e = """
+연결 실패
+"""
+
+
+def get_key(settings):
+    if os.name == 'nt':
+        return msvcrt.getch().decode('utf-8')
+    tty.setraw(sys.stdin.fileno())
+    rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
+    if rlist:
+        key = sys.stdin.read(1)
+    else:
+        key = ''
+
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    return key
+
+
+def print_vels(target_linear_velocity, target_angular_velocity):
+    print('현재 상태 : \t선속도 {0}\t 회전속도 {1} '.format(
+        target_linear_velocity,
+        target_angular_velocity))
+
+
+def make_simple_profile(output, input, slop):
+    if input > output:
+        output = min(input, output + slop)
+    elif input < output:
+        output = max(input, output - slop)
+    else:
+        output = input
+
+    return output
+
+
+def constrain(input_vel, low_bound, high_bound):
+    if input_vel < low_bound:
+        input_vel = low_bound
+    elif input_vel > high_bound:
+        input_vel = high_bound
+    else:
+        input_vel = input_vel
+
+    return input_vel
+
+
+def check_linear_limit_velocity(velocity):
+    if RIDER_MODEL == 'basic':
+        return constrain(velocity, -BASIC_MAX_LIN_VEL, BASIC_MAX_LIN_VEL)
+
+def check_angular_limit_velocity(velocity):
+    if RIDER_MODEL == 'basic':
+        return constrain(velocity, -BASIC_MAX_ANG_VEL, BASIC_MAX_ANG_VEL)
+
+
+def main():
+    settings = None
+    if os.name != 'nt':
+        settings = termios.tcgetattr(sys.stdin)
+
+    rclpy.init()
+
+    qos = QoSProfile(depth=10)
+    node = rclpy.create_node('teleop_keyboard')
+    pub = node.create_publisher(Twist, 'cmd_vel', qos)
+
+    status = 0
+    target_linear_velocity = 0.0
+    target_angular_velocity = 0.0
+    control_linear_velocity = 0.0
+    control_angular_velocity = 0.0
+
+    try:
+        print(msg)
+        while(1):
+            key = get_key(settings)
+            if key == 'w':
+                target_linear_velocity =\
+                    check_linear_limit_velocity(target_linear_velocity + LIN_VEL_STEP_SIZE)
+                status = status + 1
+                print_vels(target_linear_velocity, target_angular_velocity)
+            elif key == 'x':
+                target_linear_velocity =\
+                    check_linear_limit_velocity(target_linear_velocity - LIN_VEL_STEP_SIZE)
+                status = status + 1
+                print_vels(target_linear_velocity, target_angular_velocity)
+            elif key == 'a':
+                target_angular_velocity =\
+                    check_angular_limit_velocity(target_angular_velocity + ANG_VEL_STEP_SIZE)
+                status = status + 1
+                print_vels(target_linear_velocity, target_angular_velocity)
+            elif key == 'd':
+                target_angular_velocity =\
+                    check_angular_limit_velocity(target_angular_velocity - ANG_VEL_STEP_SIZE)
+                status = status + 1
+                print_vels(target_linear_velocity, target_angular_velocity)
+            elif key == ' ' or key == 's':
+                target_linear_velocity = 0.0
+                control_linear_velocity = 0.0
+                target_angular_velocity = 0.0
+                control_angular_velocity = 0.0
+                print_vels(target_linear_velocity, target_angular_velocity)
+            else:
+                if (key == '\x03'):
+                    break
+
+            if status == 20:
+                print(msg)
+                status = 0
+
+            twist = Twist()
+
+            control_linear_velocity = make_simple_profile(
+                control_linear_velocity,
+                target_linear_velocity,
+                (LIN_VEL_STEP_SIZE / 2.0))
+
+            twist.linear.x = control_linear_velocity
+            twist.linear.y = 0.0
+            twist.linear.z = 0.0
+
+            control_angular_velocity = make_simple_profile(
+                control_angular_velocity,
+                target_angular_velocity,
+                (ANG_VEL_STEP_SIZE / 2.0))
+
+            twist.angular.x = 0.0
+            twist.angular.y = 0.0
+            twist.angular.z = control_angular_velocity
+
+            pub.publish(twist)
+
+    except Exception as e:
+        print(e)
+
+    finally:
+        twist = Twist()
+        twist.linear.x = 0.0
+        twist.linear.y = 0.0
+        twist.linear.z = 0.0
+
+        twist.angular.x = 0.0
+        twist.angular.y = 0.0
+        twist.angular.z = 0.0
+
+        pub.publish(twist)
+
+        if os.name != 'nt':
+            termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+
+
+if __name__ == '__main__':
+    main()
---
+++ rider_teleop/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'rider_teleop'
+
+setup(
+    name=package_name,
+    version='0.0.2',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Sung',
+    maintainer_email='doney0222@naver.com',
+    description='ROS 2 Project : rider_teleop',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'teleop_keyboard = rider_teleop.script.teleop_keyboard:main'
+        ],
+    },
+)
